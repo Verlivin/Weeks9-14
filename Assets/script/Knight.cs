d5bd1a5c9c463bc6c7c6997210354e24f8eaf3c2
Daniel Heng <hengjia@shernet.sheridancollege.ca>
2026-03-23 10:42:42 -0400

    player moved

--- Assets/script/Knight.cs
+++ Assets/script/Knight.cs
@@ -1,22 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Knight : MonoBehaviour
 {
     public AudioSource SFX;
+    public float speed;
+    public Vector2 movement;
+    public Animator animator;
+    public SpriteRenderer sr;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
+        animator.SetFloat("Speed", movement.magnitude);
+        if (movement.x < 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
     }
     public void Footsteps()
     {
-        Debug.Log("step!");
+        //Debug.Log("step!");
         SFX.Play();
     }
-}
+    public void OnMove(InputAction.CallbackContext context)
+    {
+       movement = context.ReadValue<Vector2>();
+    }
+    }
+
