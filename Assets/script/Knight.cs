749e08696c10d25ef47ed8a9057ce83f8a99db1c
Verlivin <dan1829647051@gmail.com>
2026-04-05 17:12:18 -0400

    I did it!

--- Assets/script/Knight.cs
+++ Assets/script/Knight.cs
@@ -5,7 +5,8 @@ using UnityEngine.InputSystem;
 public class Knight : MonoBehaviour
 {
     public AudioSource SFX;
-    public float speed;
+	public AudioSource SFX2;
+	public float speed;
     public Vector2 movement;
     public Animator animator;
     public SpriteRenderer sr;
@@ -15,11 +16,14 @@ public class Knight : MonoBehaviour
     Coroutine jumpCoroutine;
     public GameObject[] Keys; //All the keys
     public float keyRange; //player don't have step perfectly on the keys
+    public GameObject[] keyShow;
+    public Vector3 letterSpawn;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-    }
+        letterSpawn = new Vector3(-9, 0, 0);
+	}
 
     // Update is called once per frame
     void Update()
@@ -87,7 +91,8 @@ public class Knight : MonoBehaviour
             yield return null;
         }
         animator.SetBool("jump", false);
-        CheckKeys();
+		SFX2.Play();
+		CheckKeys();
     }
     public void CheckKeys(){
 		Vector2 pos = transform.position;
@@ -98,46 +103,514 @@ public class Knight : MonoBehaviour
             if (pos.x >= keyPos.x - keyRange && pos.x <= keyPos.x + keyRange && pos.y >= keyPos.y - keyRange && pos.y <= keyPos.y + keyRange)
             {
 				//this is true coding
-				if (i == 0) { Debug.Log("1"); }
-				else if (i == 1) { Debug.Log("2"); }
-				else if (i == 2) { Debug.Log("3"); }
-				else if (i == 3) { Debug.Log("4"); }
-				else if (i == 4) { Debug.Log("5"); }
-				else if (i == 5) { Debug.Log("6"); }
-				else if (i == 6) { Debug.Log("7"); }
-				else if (i == 7) { Debug.Log("8"); }
-				else if (i == 8) { Debug.Log("9"); }
-				else if (i == 9) { Debug.Log("0"); }
+				if (i == 0) 
+                {
+                    Debug.Log("1");
+                    if (letterSpawn.x >= 5)
+                    {
+                        letterSpawn.x = -8;
+                        letterSpawn.y -= 1;
+                    }
+                    else
+                    {
+                        letterSpawn.x += 1;
+                    }
+				    Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 1)
+				{
+					Debug.Log("2");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 2)
+				{
+					Debug.Log("3");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 3)
+				{
+					Debug.Log("4");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 4)
+				{
+					Debug.Log("5");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 5)
+				{
+					Debug.Log("6");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 6)
+				{
+					Debug.Log("7");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 7)
+				{
+					Debug.Log("8");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 8)
+				{
+					Debug.Log("9");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 9)
+				{
+					Debug.Log("0");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
 
 				//Q -> M
-				else if (i == 10) { Debug.Log("Q"); }
-				else if (i == 11) { Debug.Log("W"); }
-				else if (i == 12) { Debug.Log("E"); }
-				else if (i == 13) { Debug.Log("R"); }
-				else if (i == 14) { Debug.Log("T"); }
-				else if (i == 15) { Debug.Log("Y"); }
-				else if (i == 16) { Debug.Log("U"); }
-				else if (i == 17) { Debug.Log("I"); }
-				else if (i == 18) { Debug.Log("O"); }
-				else if (i == 19) { Debug.Log("P"); }
+				else if (i == 10)
+				{
+					Debug.Log("Q");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 11)
+				{
+					Debug.Log("W");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 12)
+				{
+					Debug.Log("E");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 13)
+				{
+					Debug.Log("R");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 14)
+				{
+					Debug.Log("T");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 15)
+				{
+					Debug.Log("Y");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 16)
+				{
+					Debug.Log("U");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 17)
+				{
+					Debug.Log("I");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 18)
+				{
+					Debug.Log("O");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 19)
+				{
+					Debug.Log("P");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
 
-				else if (i == 20) { Debug.Log("A"); }
-				else if (i == 21) { Debug.Log("S"); }
-				else if (i == 22) { Debug.Log("D"); }
-				else if (i == 23) { Debug.Log("F"); }
-				else if (i == 24) { Debug.Log("G"); }
-				else if (i == 25) { Debug.Log("H"); }
-				else if (i == 26) { Debug.Log("J"); }
-				else if (i == 27) { Debug.Log("K"); }
-				else if (i == 28) { Debug.Log("L"); }
+				else if (i == 20)
+				{
+					Debug.Log("A");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 21)
+				{
+					Debug.Log("S");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 22)
+				{
+					Debug.Log("D");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 23)
+				{
+					Debug.Log("F");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 24)
+				{
+					Debug.Log("G");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 25)
+				{
+					Debug.Log("H");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 26)
+				{
+					Debug.Log("J");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 27)
+				{
+					Debug.Log("K");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 28)
+				{
+					Debug.Log("L");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
 
-				else if (i == 29) { Debug.Log("Z"); }
-				else if (i == 30) { Debug.Log("X"); }
-				else if (i == 31) { Debug.Log("C"); }
-				else if (i == 32) { Debug.Log("V"); }
-				else if (i == 33) { Debug.Log("B"); }
-				else if (i == 34) { Debug.Log("N"); }
-				else if (i == 35) { Debug.Log("M"); }
+				else if (i == 29)
+				{
+					Debug.Log("Z");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 30)
+				{
+					Debug.Log("X");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 31)
+				{
+					Debug.Log("C");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 32)
+				{
+					Debug.Log("V");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 33)
+				{
+					Debug.Log("B");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 34)
+				{
+					Debug.Log("N");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
+				else if (i == 35)
+				{
+					Debug.Log("M");
+					if (letterSpawn.x >= 5)
+					{
+						letterSpawn.x = -8;
+						letterSpawn.y -= 1;
+					}
+					else
+					{
+						letterSpawn.x += 1;
+					}
+					Instantiate(keyShow[i], letterSpawn, transform.rotation);
+				}
 
 			}
         }
